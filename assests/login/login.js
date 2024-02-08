09efe365e61c4d3100793f174f74c101eecd1881
Dhruv224 <prajapatidhruv224@gmail.com>
2024-02-08 16:55:54 +0530

    Login functionality done by Dhruv

--- assests/login/login.js
+++ assests/login/login.js
@@ -1,18 +1,16 @@
-// let users = [
-//     {
-//         id: 1, 
-//         username: "dp",
-//         password: "dp",
-//         isAdmin: false
-//     },
-//     {
-//         id: 2, 
-//         username: "vp",
-//         password: "vp",
-//         isAdmin: false
-//     },
-// ];
-// localStorage.setItem("users", JSON.stringify(users));
+let users = [
+    {
+        id: 1, 
+        username: "dp",
+        password: "dp",
+    },
+    {
+        id: 2, 
+        username: "vp",
+        password: "vp",
+    },
+];
+localStorage.setItem("users", JSON.stringify(users));
 
 document.getElementById("login-form")
     .addEventListener("submit", (event) => {
@@ -23,14 +21,17 @@ document.getElementById("login-form")
         let password = document.getElementById("password")?.value;
 
         // getting all the users from the local storage
-        let users = JSON.parse(localStorage.getItem("users"));
+        let users = JSON.parse(localStorage.getItem("users")) || [];
 
+        // checking user exists or not
         let userExist = users.find(user => user.username === username && user.password === password);
         if(userExist){
-            localStorage.setItem("isAuthenticated", "true");
+            localStorage.setItem("isAuthenticated", JSON.stringify(userExist));
 
             username.value = "";
             password.value = "";
+
+            // window.location.href = "/assests/admin/admin.html";
         }else{
             alert("User not exists. plase register first");
         }
